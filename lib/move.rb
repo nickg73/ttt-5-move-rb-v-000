e5e79b753e4b7467ce37e278fdea1affb315f477
Nicolas Godbout <nickg73@gmail.com>
2018-01-31 01:32:21 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,6 +6,9 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(move)
-   index=move.to_i - 1
+def input_to_index(user_input)
+   index=user_input.to_i - 1
+ end
+
+ def move
  end
